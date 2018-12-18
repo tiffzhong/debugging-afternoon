a78d8f2c25651e3245196caf091994dae365a1a4
Tiffany Zhong <tiffanyzhong@tiffz.local>
2018-12-18 14:56:20 -0700

    found constructor typo in ShoppingCart.js and fixed Home.js

--- stage-1/src/components/Home/Home.js
+++ stage-1/src/components/Home/Home.js
@@ -1,11 +1,9 @@
-import './Home.css';
-
+import "./Home.css";
+import React, { Component } from "react";
 class Home extends Component {
-    render() {
-        return (
-            <div className="home-landing"></div>
-        )
-    }
+  render() {
+    return <div className="home-landing" />;
+  }
 }
 
 export default Home;
