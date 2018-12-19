30264ac9d948962ed9bd2ed2ebed6248f98282d4
Tiffany Zhong <tiffanyzhong@tiffz.local>
2018-12-18 17:35:01 -0700

    fixed action.payload when deleting items from the cart

--- stage-3/src/redux/reducer.js
+++ stage-3/src/redux/reducer.js
@@ -30,7 +30,7 @@ export default function reducer(state = initialState, action) {
 
     case REMOVE_FROM_SHOPPING_CART:
       let newArray = state.shoppingCart.slice();
-      newArray.splice(action.index, 1);
+      newArray.splice(action.payload, 1);
       return Object.assign({}, state, { shoppingCart: newArray });
 
     default:
